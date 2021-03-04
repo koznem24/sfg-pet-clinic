6d9cf069a8ba9a9f0d576bf9ca3a08f915b226b5
Nemtullo <koznem24@gmail.com>
2021-03-04 21:12:49 +0100

    Refactoring to commong CRUDService. Closes #5

---
+++ pet-clinic-data/src/main/java/com/koznem/sftpetclinic/services/CrudService.java
@@ -0,0 +1,15 @@
+package com.koznem.sftpetclinic.services;
+
+import java.util.Set;
+
+public interface CrudService<T, ID> {
+
+    Set<T> findAll();
+
+    T findById(ID id);
+
+    T save(T object);
+
+    void delete(T t);
+    void delele(ID id);
+}
