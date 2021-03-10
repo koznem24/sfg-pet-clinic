392a886b654bb25c439f3561134df6916a108b36
Nemtullo <koznem24@gmail.com>
2021-03-10 18:32:10 +0100

    added PetType Service, closes #29

---
+++ pet-clinic-data/src/main/java/com/koznem/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package com.koznem.sfgpetclinic.services;
+
+import com.koznem.sfgpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/koznem/sfgpetclinic/services/map/PetTypeMapService.java
@@ -0,0 +1,36 @@
+package com.koznem.sfgpetclinic.services.map;
+
+import com.koznem.sfgpetclinic.model.PetType;
+import com.koznem.sfgpetclinic.services.PetTypeService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class PetTypeMapService extends AbstractMapService<PetType, Long> implements PetTypeService {
+
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll() ;
+    }
+
+    @Override
+    public PetType findById(Long aLong) {
+        return super.findById(aLong);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(PetType petType) {
+        super.delete(petType);
+    }
+
+    @Override
+    public void deleleById(Long aLong) {
+        super.deleteById(aLong);
+    }
+}
