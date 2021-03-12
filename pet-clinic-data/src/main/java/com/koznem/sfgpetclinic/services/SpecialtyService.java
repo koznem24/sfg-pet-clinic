e4146f95a08d37f97e46779c07dd6b2bcd3034d3
Nemtullo <koznem24@gmail.com>
2021-03-12 07:13:12 +0100

    Added Specialities to Vet on startup. closes #34

--- pet-clinic-data/src/main/java/com/koznem/sfgpetclinic/services/SpecialtyService.java
+++ pet-clinic-data/src/main/java/com/koznem/sfgpetclinic/services/SpecialtyService.java
@@ -2,5 +2,5 @@ package com.koznem.sfgpetclinic.services;
 
 import com.koznem.sfgpetclinic.model.Specialty;
 
-public interface SpecialitiesService extends CrudService<Specialty,Long> {
+public interface SpecialtyService extends CrudService<Specialty,Long> {
 }
--- pet-clinic-data/src/main/java/com/koznem/sfgpetclinic/services/map/SpecialityMapService.java
+++ pet-clinic-data/src/main/java/com/koznem/sfgpetclinic/services/map/SpecialityMapService.java
@@ -1,12 +1,12 @@
 package com.koznem.sfgpetclinic.services.map;
 
 import com.koznem.sfgpetclinic.model.Specialty;
-import com.koznem.sfgpetclinic.services.SpecialitiesService;
+import com.koznem.sfgpetclinic.services.SpecialtyService;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
 @Service
-public class SpecialityMapService extends AbstractMapService<Specialty, Long> implements SpecialitiesService {
+public class SpecialityMapService extends AbstractMapService<Specialty, Long> implements SpecialtyService {
 
     @Override
     public Set<Specialty> findAll() {
