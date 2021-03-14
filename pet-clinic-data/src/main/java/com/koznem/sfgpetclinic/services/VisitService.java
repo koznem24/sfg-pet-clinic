8df00bf20563e859f257e6c6506d2c49e4b3100c
Nemtullo <koznem24@gmail.com>
2021-03-14 15:54:19 +0100

    Implemented Map Impl for Visit Service, closes #46

---
+++ pet-clinic-data/src/main/java/com/koznem/sfgpetclinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package com.koznem.sfgpetclinic.services;
+
+import com.koznem.sfgpetclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/koznem/sfgpetclinic/services/map/VisitMapService.java
@@ -0,0 +1,40 @@
+package com.koznem.sfgpetclinic.services.map;
+
+import com.koznem.sfgpetclinic.model.Visit;
+import com.koznem.sfgpetclinic.services.VisitService;
+
+import java.util.Set;
+
+public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService {
+
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Visit save(Visit object) {
+
+        if(object.getPet() == null || object.getPet().getOwner() == null ||
+                object.getPet().getId() == null || object.getPet().getOwner().getId() == null){
+            throw new RuntimeException("Invalid Visit");
+        }
+
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(Visit t) {
+        super.delete(t);
+    }
+
+    @Override
+    public void deleleById(Long aLong) {
+        deleteById(aLong);
+    }
+}
